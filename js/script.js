0b8a5bb0cff66f8193ba3555355703f65553ab15
A Vidmar <a.v.vidmar@gmail.com>
2020-02-19 12:00:40 +0100

    little script edit

--- js/script.js
+++ js/script.js
@@ -6,28 +6,12 @@ var site = document.querySelector('.site');
 var site_h = site.offsetHeight;
 var site_w = site.offsetWidth;
 
-console.log('screen height = ' + h, 'screen width = ' + w);
-console.log('site height = ' + site_h, 'site width = ' + site_w);
+// console.log('screen height = ' + h, 'screen width = ' + w);
+// console.log('site height = ' + site_h, 'site width = ' + site_w);
 
 var header = document.getElementsByClassName('header-hero')[0];
 var mobileHero = document.getElementsByClassName('hero-image')[0];
 var desktopHero = document.getElementsByClassName('hero-image')[1];
-
-console.log(header);
-
-console.log(mobileHero);
-
-console.log(desktopHero);
-
-
-if ( site_w > h * 0.7) {
-    mobileHero.classList.remove("mobile--visible");
-    mobileHero.classList.add("mobile--hidden");
-    header.style.height = h + "px";
-    desktopHero.classList.remove("desktop--hidden");
-    desktopHero.classList.add("desktop--visible");
-    checkScreenWidth()
-}
 
 function checkScreenWidth() {
     if (site_w > h * 1.5) {
@@ -49,6 +33,16 @@ function changeColour(x) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+
+    if ( site_w > h * 0.7) {
+        mobileHero.classList.remove("mobile--visible");
+        mobileHero.classList.add("mobile--hidden");
+        header.style.height = h + "px";
+        desktopHero.classList.remove("desktop--hidden");
+        desktopHero.classList.add("desktop--visible");
+        checkScreenWidth()
+    }
+
     window.addEventListener("resize", function() {
         h = window.innerHeight;
         w = window.innerWidth;
